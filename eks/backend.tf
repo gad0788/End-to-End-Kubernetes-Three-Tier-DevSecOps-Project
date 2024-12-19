36677a72ed509b48f9757aee2aaa63f677b838b6
gad <nday.gady@gmail.com>
2024-12-19 15:18:07 -0700

    End-to-end-DevSecops pipeline

---
+++ eks/backend.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = "~> 1.10.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
+  }
+  backend "s3" {
+    bucket         = "my-terraform-bucket2825207"
+    region         = "eu-north-1"
+    key            = "eks/terraform.tfstate"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
+  }
+}
+
+provider "aws" {
+  region  = var.aws-region
+}
